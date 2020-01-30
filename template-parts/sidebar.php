1261ad2d7a1f39471e4056eda31db0288bddad2a
Mampionona <devke03@gmail.com>
2020-01-30 11:17:53 +0300

    sidebar

--- template-parts/sidebar.php
+++ template-parts/sidebar.php
@@ -8,6 +8,7 @@
     <div class="list-group">
         <a class="list-group-item" href="<?= $mon_compte_url; ?>"><?= __('Tableau de bord', 'xbot17'); ?></a>
         <a class="list-group-item" href="<?= apply_filters('translated_post_link', 58); ?>"><?= __('S\'inscrire avec IronFx', 'xbot17'); ?></a>
+        <a class="list-group-item" href="<?= apply_filters('translated_post_link', 137); ?>"><?= __('Modifier mon profil', 'xbot17'); ?></a>
         <a class="list-group-item logout-url" href="<?= $logout_url; ?>"><?= __('Se déconnecter', 'xbot17'); ?></a>
     </div>
 </div>
